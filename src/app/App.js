521faec4893c2f09421c456e603d2789612381b6
Tom Dugan <thomas@aadwebb.com>
2019-07-30 16:10:51 -0500

    luigi on screen

--- src/app/App.js
+++ src/app/App.js
@@ -1,11 +1,88 @@
 import React, {Component} from 'react';
 import './App.css';
-import {bg} from '../common/Sprite';
-import sheet from '../res/sheet.png';
+import {bg, fg} from '../common/Sprite';
 import {width, height} from '../common/common';
+import { observer } from 'mobx-react';
+import { luigi1 } from '../common/Sprite';
+import { luigi2 } from '../common/Sprite';
+import { luigi0 } from '../common/Sprite';
 
-class App extends Component {
+const SpriteWrapper = observer(class SpriteWrapper extends Component {
+  render = () => {
+    const gameSprite = this.props.gameSprite;
+
+    const translate = 'translate(' + gameSprite.cx + 'px,' + gameSprite.cy + 'px)';
+
+    var style = {
+      transform: translate,
+      position: 'absolute',
+    }
+
+    return (
+
+      <div style={style}>
+        {this.props.children}
+      </div>
+    )
+  }
+})
+
+const Bg= observer(
+  class Bg extends Component {
+    render = () => {
+      return <SpriteWrapper gameSprite={this.props.bg}> {bg} </SpriteWrapper>;
+    }
+  }
+)
+
+const Fg= observer(
+  class Fg extends Component {
+    render = () => {
+      return <SpriteWrapper gameSprite={this.props.fg}> {fg} </SpriteWrapper>;
+    }
+  }
+)
+
+export const Luigi = observer(
+  class Luigi extends Component {
+
+    render = () => {
+      let wluigi;
+      switch(this.props.luigi.frame) {
+        case 1:
+        case 3:
+          wluigi = luigi1
+          break
+        case 2:
+          wluigi = luigi2
+          break
+        case 0:
+        default:
+          wluigi = luigi0
+          break
+      }
+
+      return <SpriteWrapper gameSprite={this.props.luigi}> {wluigi} </SpriteWrapper>
+    }
+  }
+)
+
+const App = observer(class App extends Component {
+  componentDidMount() {
+    this.req = window.requestAnimationFrame(this.appUpdateFrame)
+  }
+
+  appUpdateFrame = () => {
+
+    this.props.updateFrame();
+
+    this.req = window.requestAnimationFrame(this.appUpdateFrame)
+
+  }
+
   render() {
+
+    const {bgs, fgs, luigi} = this.props.store
 
     const style = {
       width: width,
@@ -14,10 +91,12 @@ class App extends Component {
 
     return (
       <div className="App" id="fakingcanvas" style={style}>
-        {bg}
+        {bgs.map( (bg) => ( <Bg bg={bg} key={bg.id} /> ) )}
+        <Luigi luigi={luigi} />
+        {fgs.map( (fg) => ( <Fg fg={fg} key={fg.id} /> ) )}
       </div>
     );
   }
-}
+})
 
 export default App;
--- src/store/store.js
+++ src/store/store.js
@@ -1,18 +1,35 @@
-import {bg} from './asset';
+import {bg, fg, luigi} from './asset';
+import {height} from '../common/common';
+import { bg_h, bg_w, fg_h, fg_w } from '../common/Sprite';
+import { action } from 'mobx';
 
-const bg1 = new bg(guid(), 0, height - bg_h)
-const bg2 = new bg(guid(), bg_w, height - bg_h)
+const bg1 = new bg(guid(), 0, height - bg_h);
+const bg2 = new bg(guid(), bg_w, height - bg_h);
+
+const fg1 = new fg(guid(), 0, height - fg_h);
+const fg2 = new fg(guid(), fg_w, height - fg_h);
 
 export const store = {
+  luigi: new luigi(guid(), 60, 0),
+  fgpos: 0,
+  frames: 1,
   bgs: [ bg1, bg2 ],
+  fgs: [ fg1, fg2 ],
 }
 
 function guid() {
   function s4() {
     return Math.floor((1 + Math.random()) * 0x10000)
       .toString(16)
-      .subString(1);
+      .substring(1);
   }
   return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
     s4() + '-' + s4() + s4() + s4();
 }
+
+export const updateFrame = action(function() {
+  store.frames++;
+  store.fgpos = (store.fgpos -2) % 14;
+  fg1.cx = store.fgpos;
+  fg2.cx = store.fgpos + fg_w;
+});
